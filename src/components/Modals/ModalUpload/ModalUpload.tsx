ff0ff9edc861dbe8d8e47f67a2607895ee6a250f
Yevhen Martianov <jonny.freeman321@gmail.com>
2021-11-11 22:01:07 +0200

    Drag and drop files upload implemented - but needs some work to do!

--- src/components/Modals/ModalUpload/ModalUpload.tsx
+++ src/components/Modals/ModalUpload/ModalUpload.tsx
@@ -5,19 +5,32 @@ const ModalUpload = () => {
   const { uploadImage } = store.imagesStoreSettings;
 
   const handleImageUpload = (e: any) => {
+    console.log("Change");
     e.preventDefault();
     const formData = new FormData();
-    formData.append("image", e.target.files[0]);
+    if (e.target.files.length) {
+      formData.append("image", e.target.files[0]);
+      uploadImage(formData);
+    }
+  };
+
+  const handleDrop = (e: any) => {
+    console.log("Drop");
+    const formData = new FormData();
+    formData.append("image", e.dataTransfer.files[0]);
     uploadImage(formData);
   };
+
   return (
     <div className={styles.modalUploadWrapper}>
       <div className={styles.modalUploadText}>Drag'n'drop your images here</div>
       <input
         type="file"
         name="uploadingFile"
+        className={styles.modalUploadArea}
         accept="image/*"
         onChange={handleImageUpload}
+        onDrop={handleDrop}
       />
     </div>
   );
